ee09add9a42641aeb20891a0ff243b664aff7b39
AllaYukhym <yukhymenko.alla@gmail.com>
2023-02-22 15:50:43 +0100

    move DB_HOST

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const DB_HOST = process.env;
+const { DB_HOST } = process.env;
 // const DB_HOST =
 //   "mongodb+srv://Alla:allayukhym@cluster0.ofh9ldu.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
