5600b00ec3be5e403c5390313c677d1cf7a7a0cb
AllaYukhym <yukhymenko.alla@gmail.com>
2023-03-09 18:09:54 +0100

    add multer and gravatar

--- routes/api/auth/auth.js
+++ routes/api/auth/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { validateBody } = require("../../../middlewares/validation");
 const authenticate = require("../../../middlewares/authenticate");
+const upload = require("../../../middlewares/upload");
 const { schemas } = require("../../../models/user");
 const ctrl = require("../../../controllers/auth");
 
@@ -9,5 +10,11 @@ router.post("/signup", validateBody(schemas.signupSchema), ctrl.registration);
 router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 router.get("/logout", authenticate, ctrl.logout);
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrl.updateAvatar
+);
 
 module.exports = { authRouter: router };
